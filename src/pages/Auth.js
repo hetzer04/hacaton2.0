0af59b22d997dd2efbf0377d3664c1e1b2a18d75
HETZER <alex_fox1704@mail.ru>
2024-10-19 00:01:45 +0500

    Changes to be committed:

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -42,6 +42,7 @@ const Auth = () => {
   return (
     <div className="flex justify-center align-middle h-screen w-40" style={{wordWrap: "break-word"}}>
       {isLogin ? <p>Hellp</p> : <p>Авторизация</p>}
+      {Data}
     </div>
   );
 };

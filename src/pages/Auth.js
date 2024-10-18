103f8b911ed93284231cb79be9b2a9595aed0934
HETZER <alex_fox1704@mail.ru>
2024-10-18 23:44:54 +0500

    Changes to be committed:

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -9,9 +9,32 @@ const Auth = () => {
   const [isLogin, setIsLogin] = useState(false);
   const [Data, setData] = useState();
 
+  function parseUrlToJSON(url) {
+    // Разбиваем строку на параметры
+    const params = new URLSearchParams(url);
+    
+    // Извлекаем параметр user и декодируем его из JSON
+    const user = JSON.parse(decodeURIComponent(params.get('user')));
+    
+    // Собираем остальные параметры
+    const chat_instance = params.get('chat_instance');
+    const chat_type = params.get('chat_type');
+    const auth_date = params.get('auth_date');
+    const hash = params.get('hash');
+
+    // Возвращаем объект
+    return {
+        user,
+        chat_instance,
+        chat_type,
+        auth_date,
+        hash
+    };
+}
+
   useEffect(() => {
     const fetchCourses = async () => {
-      const response = await getAuth(JSON.parse(window.Telegram.WebApp.initData))
+      const response = await getAuth(parseUrlToJSON(window.Telegram.WebApp.initData))
       .then((data) => {
         setData(data);
         setIsLogin(true);
